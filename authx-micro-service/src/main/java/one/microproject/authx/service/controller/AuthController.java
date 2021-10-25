4b0bd180a609cdf3baccde891d28ff44900f5554
Juraj Veverka <gergej123@gmail.com>
2021-10-25 07:18:53 +0200

    springboot version upgraded to 2.5.6

--- authx-micro-service/src/main/java/one/microproject/authx/service/controller/AuthController.java
+++ authx-micro-service/src/main/java/one/microproject/authx/service/controller/AuthController.java
@@ -1,7 +1,9 @@
 package one.microproject.authx.service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api/v1/auth")
 public class AuthController {
 }
