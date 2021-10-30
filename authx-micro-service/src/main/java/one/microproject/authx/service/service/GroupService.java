3c1ffde4f45d84535dec06ef6a3e619e03d052f4
Juraj Veverka <juraj.veverka@inter-net.sk>
2021-10-30 21:04:23 +0200

    implemented group service interface

--- authx-micro-service/src/main/java/one/microproject/authx/service/service/GroupService.java
+++ authx-micro-service/src/main/java/one/microproject/authx/service/service/GroupService.java
@@ -1,4 +1,25 @@
 package one.microproject.authx.service.service;
 
+import one.microproject.authx.common.dto.CreateGroupRequest;
+import one.microproject.authx.common.dto.GroupDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface GroupService {
+
+    GroupDto create(String projectId, CreateGroupRequest request);
+
+    List<GroupDto> getAll();
+
+    List<GroupDto> getAll(String projectId);
+
+    Optional<GroupDto> get(String projectId, String id);
+
+    void remove(String projectId, String id);
+
+    List<GroupDto> removeAll(String projectId);
+
+    void removeAll();
+
 }
--- authx-micro-service/src/main/java/one/microproject/authx/service/service/impl/GroupServiceImpl.java
+++ authx-micro-service/src/main/java/one/microproject/authx/service/service/impl/GroupServiceImpl.java
@@ -1,11 +1,54 @@
 package one.microproject.authx.service.service.impl;
 
+import one.microproject.authx.common.dto.CreateGroupRequest;
+import one.microproject.authx.common.dto.GroupDto;
 import one.microproject.authx.service.service.GroupService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 public class GroupServiceImpl implements GroupService {
 
+    @Override
+    @Transactional
+    public GroupDto create(String projectId, CreateGroupRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<GroupDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<GroupDto> getAll(String projectId) {
+        return null;
+    }
+
+    @Override
+    public Optional<GroupDto> get(String projectId, String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    @Transactional
+    public void remove(String projectId, String id) {
+
+    }
+
+    @Override
+    @Transactional
+    public List<GroupDto> removeAll(String projectId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void removeAll() {
+
+    }
 }
