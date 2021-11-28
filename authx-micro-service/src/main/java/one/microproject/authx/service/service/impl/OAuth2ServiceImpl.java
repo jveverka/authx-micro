f4fc6a3dca6d3f368f495d0fbb0e59c6ad2c4648
Juraj Veverka <gergej123@gmail.com>
2021-11-28 20:47:10 +0100

    oauth2 impl

--- authx-micro-service/src/main/java/one/microproject/authx/service/service/impl/OAuth2ServiceImpl.java
+++ authx-micro-service/src/main/java/one/microproject/authx/service/service/impl/OAuth2ServiceImpl.java
@@ -7,9 +7,13 @@ import one.microproject.authx.service.dto.JWKResponse;
 import one.microproject.authx.service.dto.ProviderConfigurationResponse;
 import one.microproject.authx.service.dto.TokenResponse;
 import one.microproject.authx.service.dto.UserInfoResponse;
+import one.microproject.authx.service.service.ClientService;
 import one.microproject.authx.service.service.OAuth2Service;
+import one.microproject.authx.service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.net.URI;
 import java.util.Optional;
 import java.util.Set;
@@ -17,18 +21,36 @@ import java.util.Set;
 @Service
 public class OAuth2ServiceImpl implements OAuth2Service {
 
+    private final UserService userService;
+    private final ClientService clientService;
+
+    @Autowired
+    public OAuth2ServiceImpl(UserService userService, ClientService clientService) {
+        this.userService = userService;
+        this.clientService = clientService;
+    }
+
     @Override
     public TokenResponse getTokenForPassword(URI issuerUri, String projectId, ClientCredentials clientCredentials, Set<String> scopes, UserCredentials userCredentials) {
+        Boolean clientOk = clientService.verifySecret(projectId, clientCredentials.id(), clientCredentials.secret());
+        Boolean userOk = userService.verifySecret(projectId, userCredentials.username(), userCredentials.password());
+        if (clientOk && userOk) {
+
+        } else {
+
+        }
         return null;
     }
 
     @Override
     public TokenResponse getTokenForClientCredentials(URI issuerUri, String projectId, ClientCredentials clientCredentials, Set<String> scopes) {
+        Boolean clientOk = clientService.verifySecret(projectId, clientCredentials.id(), clientCredentials.secret());
         return null;
     }
 
     @Override
     public TokenResponse getTokenForRefreshToken(URI issuerUri, String projectId, ClientCredentials clientCredentials, Set<String> scopes, String refreshToken) {
+        Boolean clientOk = clientService.verifySecret(projectId, clientCredentials.id(), clientCredentials.secret());
         return null;
     }
 
