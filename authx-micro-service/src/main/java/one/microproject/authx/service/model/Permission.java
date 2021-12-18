4cc8482ef68ddac89e9ad490355bf97ea06ebdba
Juraj Veverka <gergej123@gmail.com>
2021-12-18 22:04:51 +0100

    model finished

--- authx-micro-service/src/main/java/one/microproject/authx/service/model/Permission.java
+++ authx-micro-service/src/main/java/one/microproject/authx/service/model/Permission.java
@@ -18,4 +18,73 @@ public class Permission {
     private String resource;
     private String action;
 
+    public Permission() {
+    }
+
+    public Permission(String id, String permissionId, String projectId, String description, String service, String resource, String action) {
+        this.id = id;
+        this.permissionId = permissionId;
+        this.projectId = projectId;
+        this.description = description;
+        this.service = service;
+        this.resource = resource;
+        this.action = action;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(String permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
 }
--- authx-micro-service/src/main/java/one/microproject/authx/service/model/Role.java
+++ authx-micro-service/src/main/java/one/microproject/authx/service/model/Role.java
@@ -16,4 +16,55 @@ public class Role {
     private String description;
     private List<String> permissionIds;
 
+    public Role() {
+    }
+
+    public Role(String id, String roleId, String projectId, String description, List<String> permissionIds) {
+        this.id = id;
+        this.roleId = roleId;
+        this.projectId = projectId;
+        this.description = description;
+        this.permissionIds = permissionIds;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getPermissionIds() {
+        return permissionIds;
+    }
+
+    public void setPermissionIds(List<String> permissionIds) {
+        this.permissionIds = permissionIds;
+    }
+
 }
