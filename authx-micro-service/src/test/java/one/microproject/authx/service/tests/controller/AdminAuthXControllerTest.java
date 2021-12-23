51474807895843d23a5b90aba575df116183e81c
Juraj Veverka <gergej123@gmail.com>
2021-12-23 12:17:54 +0100

    tests improved

--- authx-micro-service/src/test/java/one/microproject/authx/service/tests/controller/AdminAuthXControllerTest.java
+++ authx-micro-service/src/test/java/one/microproject/authx/service/tests/controller/AdminAuthXControllerTest.java
@@ -4,6 +4,8 @@ import one.microproject.authx.common.dto.BuildProjectRequest;
 import one.microproject.authx.common.dto.CreateClientRequest;
 import one.microproject.authx.common.dto.CreateProjectRequest;
 import one.microproject.authx.common.dto.CreateUserRequest;
+import one.microproject.authx.common.dto.GroupDto;
+import one.microproject.authx.common.dto.PermissionDto;
 import one.microproject.authx.common.dto.ProjectReportDto;
 import one.microproject.authx.common.dto.ResponseMessage;
 import one.microproject.authx.common.dto.oauth2.TokenResponse;
@@ -42,7 +44,9 @@ class AdminAuthXControllerTest extends AppBaseTest {
         CreateUserRequest adminUser = new CreateUserRequest("admin-user-001", "admin@pid-001", "", "", Map.of(), Set.of(), Set.of(), "id");
         CreateClientRequest adminClient = new CreateClientRequest("admin-client-001", "", false, "", Map.of(), Set.of(), Set.of());
         CreateProjectRequest cp = new CreateProjectRequest("pid-001", "desc", Map.of(), adminUser, adminClient);
-        return new BuildProjectRequest(cp, List.of(), List.of(), List.of(), List.of(), List.of());
+        List<GroupDto> groups = List.of(new GroupDto("g-001", "pid-001", "description", Map.of()));
+        List<PermissionDto> permissions = List.of(new PermissionDto("p-001", "pid-001", "description", "service", "resource", "action"));
+        return new BuildProjectRequest(cp, groups, List.of(), permissions, List.of(), List.of());
     }
 
 
