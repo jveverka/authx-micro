33da5c67ce6d0f167d126072bc02a8925f518b3a
Juraj Veverka <gergej123@gmail.com>
2021-12-27 14:11:17 +0100

    fixed test typo

--- authx-micro-service/src/test/java/one/microproject/authx/service/tests/service/BasicAppTest.java
+++ authx-micro-service/src/test/java/one/microproject/authx/service/tests/service/BasicAppTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
-import static com.mongodb.assertions.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BasicAppTest extends AppBaseTest {
 
